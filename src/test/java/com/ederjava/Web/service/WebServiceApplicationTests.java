2258a2d1c45bc527c3c3e1b87f72a166d0e609da
Eder <edermartelinho@hotmail.com>
2023-01-26 21:30:53 -0300

    Project created

---
+++ src/test/java/com/ederjava/Web/service/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ederjava.Web.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
