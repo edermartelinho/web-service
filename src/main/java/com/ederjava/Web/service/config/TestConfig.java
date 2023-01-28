92a90921edc4f3618e2484065f2ce6f4d6fc72d9
Eder <edermartelinho@hotmail.com>
2023-01-28 19:31:53 -0300

    OrderStatus, enum

--- src/main/java/com/ederjava/Web/service/config/TestConfig.java
+++ src/main/java/com/ederjava/Web/service/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.ederjava.Web.service.entities.Order;
 import com.ederjava.Web.service.entities.User;
+import com.ederjava.Web.service.entities.enums.OrderStatus;
 import com.ederjava.Web.service.repositories.OrderRepository;
 import com.ederjava.Web.service.repositories.UserRepository;
 
@@ -30,10 +31,10 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Fulano de Tal", "Fulano@gmail.com","998888880", "123457");
 		User u3 = new User(null, "Beutrano Dias", "Beutrano@gmail.com","998888881", "123458");
 		//ISO 8601
-		Order o1 = new Order(null, Instant.parse("2023-01-19T14:34:08Z"),u1);
-		Order o2 = new Order(null, Instant.parse("2023-03-19T19:15:23Z"),u2);
-		Order o3 = new Order(null, Instant.parse("2023-04-19T11:28:48Z"),u3);
-		Order o4 = new Order(null, Instant.parse("2023-02-19T18:41:12Z"),u2);
+		Order o1 = new Order(null, Instant.parse("2023-01-19T14:34:08Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2023-03-19T19:15:23Z"),OrderStatus.SHIPED, u2);
+		Order o3 = new Order(null, Instant.parse("2023-04-19T11:28:48Z"),OrderStatus.WAITING_PAYMENT, u3);
+		Order o4 = new Order(null, Instant.parse("2023-02-19T18:41:12Z"),OrderStatus.DELIVERED, u2);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3,o4));
