fc93b9be4a5802eb1a1847bf14b5ad233a780973
Eder <edermartelinho@hotmail.com>
2023-02-01 23:46:33 -0300

    Exception handling - update

--- src/main/java/com/ederjava/Web/service/services/UserService.java
+++ src/main/java/com/ederjava/Web/service/services/UserService.java
@@ -13,6 +13,8 @@ import com.ederjava.Web.service.repositories.UserRepository;
 import com.ederjava.Web.service.services.exceptions.DatabaseException;
 import com.ederjava.Web.service.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -44,10 +46,17 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
+		
+	try {
+		
+		@SuppressWarnings("deprecation")
+		User entity = repository.getOne(id);
 		updateData(entity, obj);
 		return repository.save(entity);
-	}
+	  }catch(EntityNotFoundException e) {
+		  throw new ResourceNotFoundException(id);
+	  }
+}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
